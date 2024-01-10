39e1a3d30e6a53286948c5990d7b8e0803b5815c
Adnan-Shaikh-web <adnanshaikh090103@gmail.com>
2024-01-10 15:48:40 +0530

    Third Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,25 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './Pages/Chat';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NavBar from './Components/NavBar';
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Chat />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<Navigate to="/" />} />
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path='/' element={<Chat />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
+
   )
 }
 
