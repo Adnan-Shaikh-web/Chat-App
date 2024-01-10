e946e37ad5cdfa4a43b49951e567c61af7dbfcad
Adnan-Shaikh-web <adnanshaikh090103@gmail.com>
2024-01-10 17:07:33 +0530

    Created Functionality for Registering a User

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 const Register = () => {
+    const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } = useContext(AuthContext)
     return (
         <>
-            <Form>
+            <Form onSubmit={registerUser}>
                 <Row style={{ height: '100vh', justifyContent: 'center', paddingTop: '10%' }}>
                     <Col xs={6}>
                         <Stack gap={3}>
                             <h2>Register</h2>
-                            <Form.Control type='text' placeholder='Name' />
-                            <Form.Control type='email' placeholder='Email' />
-                            <Form.Control type='password' placeholder='password' />
+                            <Form.Control type='text' placeholder='Name' onChange={(e) => updateRegisterInfo({ ...registerInfo, name: e.target.value })} />
+                            <Form.Control type='email' placeholder='Email' onChange={(e) => updateRegisterInfo({ ...registerInfo, email: e.target.value })} />
+                            <Form.Control type='password' placeholder='password' onChange={(e) => updateRegisterInfo({ ...registerInfo, password: e.target.value })} />
                             <Button variant='primary' type='submit'>
-                                Register
+                                {isRegisterLoading ? "Creating your account" : "Register"}
                             </Button>
-                            <Alert variant='danger'><p>An error occured</p></Alert>
+                            {registerError?.error &&
+                                <Alert variant='danger'><p>{registerError?.message}</p></Alert>
+                            }
                         </Stack>
                     </Col>
                 </Row>
