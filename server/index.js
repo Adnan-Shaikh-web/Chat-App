1b745a130c96b23a27ce18b7a252fa23080b109d
Adnan-Shaikh-web <adnanshaikh090103@gmail.com>
2024-01-10 12:58:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require('./Routes/userRoute')
+require('dotenv').config()
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', userRoute);
+
+//CRUD  
+app.get('/', (req, res) => {
+    res.send('Welcome to Our Chat App Api...')
+})
+
+
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port: ${port}`)
+})
+mongoose.connect(uri).then(() => {
+    console.log('Connection successful to atlas')
+})
