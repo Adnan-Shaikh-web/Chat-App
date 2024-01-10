550ca10bcd04325cefa87df4b36cbdda40dc3523
Adnan-Shaikh-web <adnanshaikh090103@gmail.com>
2024-01-10 14:46:35 +0530

    Second Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Chat from './Pages/Chat';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Chat />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
