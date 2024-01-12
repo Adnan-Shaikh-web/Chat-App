8d1f3a7be11ce4f8a2528f88df01aca148afd24f
Adnan-Shaikh-web <adnanshaikh090103@gmail.com>
2024-01-12 12:55:18 +0530

    created functionality to store messages in the database

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoute = require('./Routes/userRoute');
 const chatRoute = require('./Routes/chatRoute');
+const messageRoute = require('./Routes/messageRoute');
 require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/users', userRoute);
 app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
 
 
 //CRUD  
